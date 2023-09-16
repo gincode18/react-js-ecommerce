324c23e5794147ed0465566545bd9f1e7686730f
gincode18 <vishalkamboj9211@gmail.com>
2023-09-16 13:26:56 +0530

    done with sort animation

--- react/src/features/product-list/ProductFilter.jsx
+++ react/src/features/product-list/ProductFilter.jsx
@@ -15,7 +15,7 @@ import {
   fetchProductAsyncSort,
   fetchProductPage,
 } from "../product-list/productSlice";
-import ProductList from "./ProductsList";
+import { AnimatePresence, motion } from "framer-motion";
 
 const sortOptions = [
   { name: "Price: Low to High", sort: "price", order: "asc", current: false },
@@ -114,6 +114,16 @@ export default function ProductFilter({ children }) {
     console.log(x);
     setfilterdata(x);
   };
+  const productState = useSelector((state) => state.product);
+  const newproducts = productState.products;
+  const status = useSelector((state) => state.product.status);
+  const [page, setPage] = useState(1);
+  useEffect(() => {
+    dispatch(fetchProductAsync(page));
+  }, [page]);
+  useEffect(() => {
+    console.log(newproducts);
+  }, [newproducts]);
   const dispatch = useDispatch();
   const [filterdata, setfilterdata] = useState({});
   const [mobileFiltersOpen, setMobileFiltersOpen] = useState(false);
@@ -125,6 +135,80 @@ export default function ProductFilter({ children }) {
       dispatch(fetchProductAsyncFilter(filterdata, ProductPage));
     }
   }, [filterdata]);
+  const ProductList = (
+    <div className="">
+      <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8 flex flex-col justify-center items-center">
+        <h2 className="text-2xl font-bold tracking-tight text-white item -translate-x-[80%] mb-16">
+          Customers also purchased
+        </h2>
+
+        <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
+          <AnimatePresence>
+            {newproducts.map((product) => (
+              <motion.div
+                layout
+                key={product.id}
+                className=""
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                exit={{ opacity: 0 }}
+                transition={{ duration: 0.5 }}
+              >
+                {/* <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80"> */}
+                <img
+                  src={product.images[0]}
+                  alt={product.title}
+                  className="h-full w-full object-cover object-center lg:h-full lg:w-full"
+                />
+                {/* </div> */}
+                <div className="mt-4 flex justify-between">
+                  <div>
+                    <h3 className="text-sm  text-white">
+                      <a href={product.thumbnail}>
+                        <span aria-hidden="true" className=" " />
+                        {product.title}
+                      </a>
+                    </h3>
+                    <p className="mt-1 text-sm text-white">
+                      {product.discountPercentage}
+                    </p>
+                  </div>
+                  <p className="text-sm font-medium text-white">
+                    {product.price}
+                  </p>
+                </div>
+              </motion.div>
+            ))}
+          </AnimatePresence>
+        </div>
+        <div className="join mt-24">
+          <button
+            className="join-item btn"
+            onClick={() => {
+              if (page > 1) {
+                dispatch(fetchProductPage(page - 1));
+                setPage(page - 1);
+              }
+            }}
+          >
+            «
+          </button>
+          <button className="join-item btn">Page {page}</button>
+          <button
+            className="join-item btn"
+            onClick={() => {
+              if (page < 5) {
+                dispatch(fetchProductPage(page + 1));
+                setPage(page + 1);
+              }
+            }}
+          >
+            »
+          </button>
+        </div>
+      </div>
+    </div>
+  );
   return (
     <div className="">
       <div>
@@ -398,7 +482,7 @@ export default function ProductFilter({ children }) {
 
               {/* Product grid */}
               <div className="lg:col-span-3">
-                <ProductList></ProductList>
+                {ProductList}
               </div>
             </div>
           </section>
--- react/src/features/product-list/ProductsList.jsx
+++ react/src/features/product-list/ProductsList.jsx
@@ -1,23 +1,24 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
+import { Fragment, useEffect, useState } from "react";
+import { Dialog, Disclosure, Menu, Transition } from "@headlessui/react";
+import { XMarkIcon } from "@heroicons/react/24/outline";
+import {
+  ChevronDownIcon,
+  FunnelIcon,
+  MinusIcon,
+  PlusIcon,
+  Squares2X2Icon,
+} from "@heroicons/react/20/solid";
 import { useDispatch, useSelector } from "react-redux";
-import { fetchProductAsync, fetchProductPage } from "./productSlice";
-
-export default function ProductList() {
-  const dispatch = useDispatch();
-  const productState = useSelector((state) => state.product);
-  const newproducts = productState.products;
-  const status = useSelector((state) => state.product.status);
-  const [page, setPage] = useState(1);
-  useEffect(() => {
-    dispatch(fetchProductAsync(page));
-  }, [page]);
-  useEffect(() => {
-    console.log(newproducts);
-  }, [newproducts]);
-
-  if (status === "loading") {
-    return <div>Loading...</div>;
-  }
+import {
+  fetchProductAsync,
+  fetchProductAsyncFilter,
+  fetchProductAsyncSort,
+  fetchProductPage,
+} from "../product-list/productSlice";
+// import ProductsList from "./ProductsList";
+import { AnimatePresence, motion } from "framer-motion";
+function ProductsList() {
   return (
     <div className="">
       <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8 flex flex-col justify-center items-center">
@@ -26,35 +27,45 @@ export default function ProductList() {
         </h2>
 
         <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
-          {newproducts.map((product) => (
-            <div key={product.id} className="group relative">
-              <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80">
+          <AnimatePresence>
+            {newproducts.map((product) => (
+              <motion.div
+                layout
+                key={product.id}
+                className=""
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                exit={{ opacity: 0 }}
+                transition={{ duration: 0.5 }}
+              >
+                {/* <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80"> */}
                 <img
                   src={product.images[0]}
                   alt={product.title}
                   className="h-full w-full object-cover object-center lg:h-full lg:w-full"
                 />
-              </div>
-              <div className="mt-4 flex justify-between">
-                <div>
-                  <h3 className="text-sm  text-white">
-                    <a href={product.thumbnail}>
-                      <span aria-hidden="true" className="absolute inset-0" />
-                      {product.title}
-                    </a>
-                  </h3>
-                  <p className="mt-1 text-sm text-white">
-                    {product.discountPercentage}
+                {/* </div> */}
+                <div className="mt-4 flex justify-between">
+                  <div>
+                    <h3 className="text-sm  text-white">
+                      <a href={product.thumbnail}>
+                        <span aria-hidden="true" className=" " />
+                        {product.title}
+                      </a>
+                    </h3>
+                    <p className="mt-1 text-sm text-white">
+                      {product.discountPercentage}
+                    </p>
+                  </div>
+                  <p className="text-sm font-medium text-white">
+                    {product.price}
                   </p>
                 </div>
-                <p className="text-sm font-medium text-white">
-                  {product.price}
-                </p>
-              </div>
-            </div>
-          ))}
+              </motion.div>
+            ))}
+          </AnimatePresence>
         </div>
-        <div className="join mt-16">
+        <div className="join mt-24">
           <button
             className="join-item btn"
             onClick={() => {
@@ -83,3 +94,5 @@ export default function ProductList() {
     </div>
   );
 }
+
+export default ProductsList;
