57ffcce3622c35bc64232f8eaaa71c24d41159f1
gincode18 <vishalkamboj9211@gmail.com>
2023-09-23 17:21:27 +0530

    Removed Old Code :)

--- backend/controller/logoutController.js
+++
@@ -1,9 +0,0 @@
-import User from '../model/userSchema'
-
-export const logout=(req, res) => {
-    res.cookie("token", null, {
-      httpOnly: true,
-      expires: new Date(Date.now()),
-    });
-    res.redirect("/");
-  }
