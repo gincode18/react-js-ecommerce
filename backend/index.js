206fe0f3b71c5cae2997019518746a75d685c345
gincode18 <vishalkamboj9211@gmail.com>
2023-09-24 09:59:15 +0530

    Done with basic template

---
+++ backend/index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const Product =require('./routes/Product.js') // Check the path to your Product module
+
+const app = express();
+
+const db = async () => {
+  try {
+    await mongoose.connect("mongodb+srv://vishalkamboj9211:vishal1234@cluster0.tqdqd4j.mongodb.net/?retryWrites=true&w=majority", {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("DB is connected");
+  } catch (error) {
+    console.error("Error connecting to the database:", error);
+  }
+};
+
+db();
+
+// Make sure to use "app", not "server"
+app.use(express.json());
+app.use("/products", Product); // Use the imported router
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
