8992b6ec03d010ec3e4c28e845f9bfb9bba3f4d3
gincode18 <vishalkamboj9211@gmail.com>
2023-11-20 22:49:35 +0530

    :)

--- backend/index.js
+++ backend/index.js
@@ -83,7 +83,7 @@ app.use(function(req, res, next) {
   const allowedOrigins = ['http://localhost:5173', 'http://gamebrag.onrender.com', 'https://gamebrag.onrender.com'];
   const origin = req.headers.origin;
   if (allowedOrigins.includes(origin)) {
-       res.setHeader('Access-Control-Allow-Origin', "*");
+       res.setHeader('Access-Control-Allow-Origin', origin);
   }
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
   res.header("Access-Control-Allow-credentials", true);
